0ad81cd1541fc5a04cde6bb70c9ba209703a21b4
Shubham Gupta <shubham0209@gmail.com>
2017-12-21 14:21:23 +0530

    First commit

---
+++ Project/Assets/Scripts/camChase.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class camChase : MonoBehaviour {
+
+	private Vector3 offset;
+
+	void Start () {
+		offset = transform.position - GetCentre();
+	}
+
+	void LateUpdate () {
+		transform.position = offset + GetCentre();
+	}
+
+	Vector3 GetCentre () {
+		Vector3 centre = Vector3.zero;
+		int count = 0;
+		foreach(GameObject obj in GameObject.FindGameObjectsWithTag("Player")){
+			count++;
+			centre += obj.transform.position;
+		}
+		return centre/count;
+	}
+}
---
+++ Project/Assets/Scripts/move.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class move : MonoBehaviour {
+
+	public float initialVelocity, acceleration;
+
+	private Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		rb.AddForce(transform.forward * initialVelocity, ForceMode.VelocityChange);
+	}
+
+	void FixedUpdate () {
+		rb.AddForce(transform.forward * acceleration, ForceMode.Acceleration);
+	}
+
+	void OnTriggerEnter (Collider col) {
+		GameObject.Find("EndText").GetComponent<Text>().text = "Time taken to collide: " + Time.time.ToString() + "s";
+		Time.timeScale = 0f;
+	}
+}
